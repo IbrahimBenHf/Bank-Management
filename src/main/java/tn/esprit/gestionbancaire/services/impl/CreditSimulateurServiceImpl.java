5cc9f410902b705afb0d2fae15e8ba7ef087c31d
Zied Boulifi <zied.boulifi@esprit.tn>
2021-06-30 21:44:07 +0100

    Fix credit Vehicle Simulateur

--- src/main/java/tn/esprit/gestionbancaire/services/impl/CreditSimulateurServiceImpl.java
+++ src/main/java/tn/esprit/gestionbancaire/services/impl/CreditSimulateurServiceImpl.java
@@ -1,12 +1,11 @@
 package tn.esprit.gestionbancaire.services.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tn.esprit.gestionbancaire.exception.ErrorCodes;
-import tn.esprit.gestionbancaire.exception.InvalidEntityException;
 import tn.esprit.gestionbancaire.exception.InvalidOperationException;
 import tn.esprit.gestionbancaire.services.CreditSimulateurService;
-import static tn.esprit.gestionbancaire.utils.Constants.DEFAULT_RATE;
 import static tn.esprit.gestionbancaire.utils.Constants.MIN_SELF_FINANCING;
 
 import java.util.HashMap;
@@ -14,6 +13,8 @@ import java.util.Map;
 @Service
 @Slf4j
 public class CreditSimulateurServiceImpl implements CreditSimulateurService {
+    @Value("${user.simple.rate}")
+    double simpleUserRate;
 
     @Override
     public String creditMedia(Integer creditAmout, Integer repaymentPeriod) {
@@ -22,21 +23,22 @@ public class CreditSimulateurServiceImpl implements CreditSimulateurService {
 
     @Override
     public Map<Integer, Double> creditVehicle(double vehicleAmout, Integer vehicleFiscalPower, double selfFinancing, Integer repaymentPeriod) {
+
+
         if (selfFinancing < vehicleAmout * MIN_SELF_FINANCING) {
             log.error("Insufficient Self Financing", selfFinancing);
             throw new InvalidOperationException("Self Financing must be >= 20% of vehicle Amout !!", ErrorCodes.CREDIT_SIMILATEUR_SELF_FINANCING_INSUFFICIENT);
         }
-        Map<Integer,Double> map = new HashMap<Integer,Double>();
+        Map<Integer,Double> map = new HashMap<>();
         double creditAmout = vehicleAmout - selfFinancing;
         double rest = creditAmout;
         double fixedRepayment = creditAmout / repaymentPeriod;
         double monthRepayment = 0.0;
         for(int i = 0; i < repaymentPeriod ; i++){
-            monthRepayment = fixedRepayment + (rest * DEFAULT_RATE);
+            monthRepayment = fixedRepayment + (rest * simpleUserRate);
             map.put(i+1,monthRepayment);
             rest = rest - monthRepayment;
         }
-        map.entrySet().stream().forEach(e-> System.out.println(e));
         return map;
     }
 
