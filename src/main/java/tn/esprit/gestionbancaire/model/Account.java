25c225db984fdb3f7b788f88aee1931dfad37edb
ibenhfaiedh <ibenhfaiedh@vermeg.com>
2021-07-09 22:19:57 +0100

    commit some fixes reclamation

--- src/main/java/tn/esprit/gestionbancaire/model/Account.java
+++ src/main/java/tn/esprit/gestionbancaire/model/Account.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -23,7 +24,7 @@ public abstract class Account implements Serializable {
 
     private Date creationDate;
 
-    private float balance;
+    private BigDecimal balance;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
