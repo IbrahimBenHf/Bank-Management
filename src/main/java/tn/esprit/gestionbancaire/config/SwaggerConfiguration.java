797319ddb9fd9365a3d2f6c87296dadecfd04a78
adly <adly.nefzaoui@esprit.tn>
2021-07-09 23:55:52 +0100

    Fix Swagger with JWT

--- src/main/java/tn/esprit/gestionbancaire/config/SwaggerConfiguration.java
+++ src/main/java/tn/esprit/gestionbancaire/config/SwaggerConfiguration.java
@@ -1,13 +1,22 @@
 package tn.esprit.gestionbancaire.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -25,6 +34,8 @@ public class SwaggerConfiguration {
                 .title("Gestion bancaire REST API")
                 .build()
         )
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
         .groupName("REST API V1")
         .select()
         .apis(RequestHandlerSelectors.basePackage("tn.esprit.gestionbancaire"))
@@ -32,5 +43,20 @@ public class SwaggerConfiguration {
         .build(); 
   }
 
+
+  private ApiKey apiKey() {
+    return new ApiKey("JWT", "Authorization", "header");
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder().securityReferences(defaultAuth()).build();
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+  }
 
 }
