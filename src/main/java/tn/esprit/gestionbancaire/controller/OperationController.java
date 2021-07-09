560128816ee97b8cfc49847c0ab2e85262dfa903
adly <adly.nefzaoui@esprit.tn>
2021-07-09 23:56:35 +0100

    OperationAPIs fix

--- src/main/java/tn/esprit/gestionbancaire/controller/OperationController.java
+++ src/main/java/tn/esprit/gestionbancaire/controller/OperationController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esprit.gestionbancaire.controller.api.OperationAPI;
+import tn.esprit.gestionbancaire.enums.OperationStatus;
 import tn.esprit.gestionbancaire.model.Currency;
 import tn.esprit.gestionbancaire.model.Operation;
 import tn.esprit.gestionbancaire.services.ICurrencyService;
 import tn.esprit.gestionbancaire.services.IOperationService;
+
+import java.util.List;
 
 @RestController
 public class OperationController implements OperationAPI {
@@ -16,5 +19,35 @@ public class OperationController implements OperationAPI {
     @Override
     public ResponseEntity<Operation> save(Operation operation) {
         return ResponseEntity.ok(operationService.save(operation));
+    }
+
+    @Override
+    public ResponseEntity<Operation> updateCreditStatus(Integer idCredit, OperationStatus operationStatus) {
+        return ResponseEntity.ok(operationService.updateOperationStatus(idCredit,operationStatus));
+    }
+
+    @Override
+    public Operation getOperationById(Integer id) {
+        return operationService.findOperationById(id);
+    }
+
+    @Override
+    public List<Operation> findAllByArchived(Boolean archived) {
+        return operationService.getArchivedOperation(archived);
+    }
+
+    @Override
+    public List<Operation> findAllByUser(Integer id) {
+        return operationService.getAllOperationByClient(id);
+    }
+
+    @Override
+    public List<Operation> getAllOperationByClientAndStatus(Integer id, OperationStatus operationStatus) {
+        return operationService.getAllOperationByClientAndStatus(id,operationStatus);
+    }
+    // TODO Tomorrow
+    @Override
+    public ResponseEntity<Operation> revert(Integer idOperation) {
+        return null;
     }
 }
