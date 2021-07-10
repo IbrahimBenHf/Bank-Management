8495aced3fb8ecf0ab6cb95479ed59b3dc81732f
X415795 <a.rojbeni@servexternos.gruposantander.com>
2021-07-10 09:25:58 +0100

    add import

--- src/main/java/tn/esprit/gestionbancaire/repository/ClientRepository.java
+++ src/main/java/tn/esprit/gestionbancaire/repository/ClientRepository.java
@@ -1,6 +1,8 @@
 package tn.esprit.gestionbancaire.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import tn.esprit.gestionbancaire.model.Client;
 
 @Repository
